c03b579ab00bec0a61fdf909d76b6a3b2c0da228
JayJayOi <45645597+JayJayOi@users.noreply.github.com>
2019-08-06 15:59:12 +0800

    Updated

--- Bash/Modules/3.7_Summary.sh
+++ Bash/Modules/3.7_Summary.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #3.7 Summary
-
 echo -e "\e[31;43m*****Scan Summary*****\e[0m"
 echo "Scan Has Been Completed, The scan summary has been compiled into a txt file with the name as shown below:"
 ls -l $newDir
